dd9a7bffbf8e959d88c3eab916215a8cb76781c4
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2026-01-16 03:42:24 -0300

    feat: add rota GET /sales/:saleId

--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { listSales } from '../controllers/sales.controller.js';
+import { listSales, getSale } from '../controllers/sales.controller.js';
 
 const router = Router();
 
 router.get('/', listSales);
+router.get('/:saleId', getSale);
 
 export default router;
